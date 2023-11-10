2726139c916d8dc2c91105aff1cba224f1633145
hugo <hugo.lm2707@gmail.com>
2023-11-10 01:14:45 +0100

    add custom section component

---
+++ components/global/Section.vue
@@ -0,0 +1,73 @@
+<style lang="scss" scoped>
+
+.section {
+  font-family: Sora;
+  
+  min-height: 100vh;
+}
+.title {
+  color: var(--secondary-base, #1BD9FF);
+  text-align: center;
+
+  font-size: 21px;
+  font-weight: 600;
+  line-height: 140%;
+  letter-spacing: 1.26px;
+  text-transform: uppercase;
+
+  margin-bottom: 8px;
+}
+
+.subtitle {
+  color: var(--grey-100, #FFF);
+  text-align: center;
+
+  font-size: 51px;
+  font-weight: 700;
+  line-height: 120%;
+  letter-spacing: -0.51px;
+
+  margin-bottom: 48px;
+}
+</style>
+
+<template>
+  <div class="section" align="center">
+    <h5 class="title">
+      <slot name="title">{{ title }}</slot>
+    </h5>
+
+    <h2 class="subtitle">
+      <slot name="subtitle">{{ subtitle }}</slot>
+    </h2>
+
+    <slot />
+
+  </div>
+</template>
+
+<script>
+export default {
+  components: { },
+  props: { 
+    title: {
+      type: String,
+      default: 'Title'
+    },
+    subtitle: {
+      type: String,
+      default: 'Subtitle'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
--- plugins/vue-components.js
+++ plugins/vue-components.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 
 import Page from '@/components/global/Page.vue'
+import Section from '@/components/global/Section.vue'
 
 Vue.component('Page', Page)
+Vue.component('Section', Section)
 
