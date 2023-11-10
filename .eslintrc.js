2ca79cf5559985bf52edee893e19dd5a00ad42e8
Hugo LM <hugo.lm2707>
2023-11-10 15:45:04 +0100

    Update ESLint rules and Docker Compose ports and volumes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,14 @@ module.exports = {
     'prettier',
   ],
   plugins: [],
-  // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 'off',
+    'vue/first-attribute-linebreak': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/component-tags-order': [
+      'warn', {
+        order: ['style', 'template', 'script'],
+      },
+    ],
+  },
 }
